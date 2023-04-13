60b4ed4164e9fead191d357cd3e3bd0232ec6317
kifle23 <68891198+kifle23@users.noreply.github.com>
2023-04-13 10:37:28 +0300

    Add local storage

--- scripts/validation-handler.js
+++ scripts/validation-handler.js
@@ -1,14 +1,22 @@
-const errorElement = document.querySelector('.form-validation-msg');
+const errorElement = document.querySelector('#alert');
 const emailInput = document.querySelector('input[type="email"]');
+const fullNameInputv = document.querySelector('#full-name');
+const messageInputv = document.querySelector('#form-message');
 const submit = document.querySelector('#btn-msg');
 
 function showError(event) {
+  errorElement.className = '';
   if (emailInput.value !== emailInput.value.toLowerCase()) {
+    errorElement.classList.add('form-validation-msg');
     errorElement.textContent = '*The email address should be in lowercase.';
     event.preventDefault();
-  }
-  if (!emailInput.validity.valid) {
-    errorElement.textContent = '*Please enter the correct email address.';
+  } else if (
+    emailInput.value === ''
+    || fullNameInputv.value === ''
+    || messageInputv.value === ''
+  ) {
+    errorElement.classList.add('form-validation-msg');
+    errorElement.textContent = '*All fields are required! Please enter a value';
     event.preventDefault();
   }
   setTimeout(() => {
